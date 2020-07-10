6b18f8701b21ae2af757828133dc14141a8854cd
Suraj2232001 <67924985+Suraj2232001@users.noreply.github.com>
2020-07-11 01:25:30 +0530

    project task 9 javascript

---
+++ project 4.js
@@ -0,0 +1,32 @@
+var registeredEarly = false;
+if(registeredEarly)
+    {
+        console.log (' registered early');
+}
+ else {
+     console.log (' not registered');
+}
+var runnersAge =14 ; 
+let raceNumber = Math.floor(Math.random() * 1000);
+if(runnersAge > 18 && registeredEarly)
+{ 
+    raceNumber+=1000;
+}
+else {
+    raceNumber = raceNumber;
+}
+if(runnersAge > 18 && registeredEarly)
+{console.log (' u will race at 9:30 am.');
+console.log(`your raceNumber is ${raceNumber}`);
+}
+else if (runnersAge > 18 && notRegistered)
+{console.log(' u will race at 11:00 am.');
+console.log(`your raceNumber is ${raceNumber}`);
+}
+else if(runnersAge < 18)
+{console.log(' u will race at 12:30 pm.');
+console.log(`your raceNumber is ${raceNumber}`);
+}
+else
+{console.log(' go to the registration desk.');
+}
